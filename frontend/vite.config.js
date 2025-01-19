5942c36eea061ae03e36d7bc6c8aa25b8f417a34
Vasantkr97 <vasantkr97@gmail.com>
2025-01-19 17:12:15 +0530

    some change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Use a separate directory for the build output
-  },
-  publicDir: 'public', // Ensure the `public` directory is used only for static assets
+  
 });
